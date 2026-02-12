31206f3556f915082c8ec1ba7c005a32682929e9
Ian Cheung <iancheung@ocf.berkeley.edu>
2026-02-11 17:38:03 -0800

    added dock

--- home.nix
+++ home.nix
@@ -3,7 +3,20 @@
 {
   home.username = "iancheung";
   home.homeDirectory = "/home/i/ia/iancheung";
-  home.stateVersion = "25.11"; 
+  home.stateVersion = "25.11";
+
+  # Remove all contents from ~/Desktop
+  home.activation.clearDesktop = config.lib.dag.entryAfter ["writeBoundary"] ''
+    $DRY_RUN_CMD rm -rf ${config.home.homeDirectory}/Desktop/*
+  '';
+
+  # Set ~/remote as true desktop
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+    desktop = "${config.home.homeDirectory}/remote";
+    download = "${config.home.homeDirectory}/remote";
+  };
 
   home.packages = [
     pkgs.whitesur-kde
@@ -13,16 +26,17 @@
   ];
 
   programs.plasma = {
-    enable = true;  
+    enable = true;
     overrideConfig = true;
+
+    desktop.mouseActions.rightClick = "contextMenu";
 
     workspace = {
       lookAndFeel = "com.github.vinceliuice.WhiteSur-dark";
-      iconTheme = "WhiteSur-dark"; 
+      iconTheme = "WhiteSur-dark";
       cursor.theme = "macOS";
       wallpaper = "${./wallpaper.jpg}";
 
-    # This is the nested path that should work for your version
       windowDecorations = {
         library = "org.kde.kwin.aurorae";
         theme = "WhiteSur-dark";
@@ -32,18 +46,57 @@
     panels = [
       {
         location = "top";
-        height = 30;
+        height = 35;
         widgets = [
           "org.kde.plasma.kickoff"
-          "org.kde.plasma.appmenu" 
+          "org.kde.plasma.appmenu"
           "org.kde.plasma.panelspacer"
           "org.kde.plasma.systemtray"
           "org.kde.plasma.digitalclock"
         ];
       }
+      {
+        location = "bottom";
+        height = 64;
+        alignment = "center";
+        lengthMode = "fit";
+        floating = true;
+        hiding = "none"; # autohide
+        widgets = [
+          {
+            iconTasks = {
+              launchers = [
+                "applications:org.gnome.Nautilus.desktop"    # Finder
+                "applications:xfce4-web-browser.desktop"     # Safari
+                "applications:google-chrome.desktop"         # Chrome
+                "applications:firefox.desktop"               # Firefox
+                "applications:org.kde.kate.desktop"          # Kate
+                "applications:Zoom.desktop"                  # Zoom
+                "applications:org.inkscape.Inkscape.desktop" # Inkscape
+                "applications:idea-oss.desktop"              # IntelliJ
+                "applications:code.desktop"                  # VS Code
+                "applications:systemsettings.desktop"        # Settings
+                "applications:org.kde.kwrite.desktop"        # Text Editor
+                "applications:org.kde.konsole.desktop"       # Terminal
+                "applications:xfce4-taskmanager.desktop"     # Task Manager
+              ];
+            };
+          }
+          {
+            name = "org.kde.plasma.folder";
+            config = {
+              General = {
+                url = "file:///home/i/ia/iancheung/remote";
+                label = "Remote";
+              };
+            };
+          }
+          # "org.kde.plasma.icontasks" # Recently Used
+          "org.kde.plasma.trash"       # Trash
+        ];
+      }
     ];
 
-    # This forces the macOS button alignment (Close/Min/Max on the left)
     configFile."kwinrc"."org.kde.kwin"."titlebarButtonsLeft" = "XAI";
   };
 
@@ -57,4 +110,3 @@
 
   programs.home-manager.enable = true;
 }
-
